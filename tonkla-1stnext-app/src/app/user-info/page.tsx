4bdec356343c4041d729fb7b1e41292ffefccbd1
RisingTonklaman <118422158+RisingTonklaman@users.noreply.github.com>
2025-05-27 14:33:38 +0700

    user-into

--- tonkla-1stnext-app/src/app/user-info/page.tsx
+++ tonkla-1stnext-app/src/app/user-info/page.tsx
@@ -1,48 +1,58 @@
 "use client";
 import React from "react";
 import { FaBolt } from "react-icons/fa";
+import RedButton from "@/components/reusecomponents/RedButton";
 
 export default function ChargingSession() {
-  const boxClass = "h-[100%] rounded-xl py-2 text-center";
-
   return (
-    <div className="flex flex-col flex-grow">
+    <div className="h-screen flex flex-col justify-between">
       {/* Header */}
-      <div className="relative">
-        {/* Circle Progress absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 */}
-        <div className="flex justify-center items-center h-[40vh]">
-          <div className="w-48 h-48 rounded-full border-[12px] border-teal-400 border-t-0 border-b-0 flex flex-col items-center justify-center bg-black text-white shadow-xl">
-            <FaBolt className="text-cyan-400 text-xl mb-1" />
-            <span className="text-cyan-400 text-xl font-semibold">70%</span>
-            <span className="text-4xl font-bold">500</span>
-            <span className="text-sm">km</span>
-          </div>
+      <div className="flex justify-center items-center h-[45vh]">
+        <div className="w-48 h-48 rounded-full border-[12px] border-teal-400 border-t-0 border-b-0 flex flex-col items-center justify-center bg-black text-white shadow-xl">
+          <FaBolt className="text-cyan-400 text-xl mb-1" />
+          <span className="text-cyan-400 text-xl font-semibold">70%</span>
+          <span className="text-4xl font-bold">500</span>
+          <span className="text-sm">km</span>
         </div>
       </div>
 
-      {/* Bottom Section mt-auto */}
-      <div className="mt-auto w-full min-h-[44vh]  bg-gradient-to-b from-teal-600 to-black text-white rounded-t-2xl rounded-b-none shadow-lg p-4 flex flex-col">
-        <p className="text-sm font-semibold">
-          ชื่อสถานนี : <span className="font-light">ห้องชาร์จ</span>
-        </p>
-        <div className="mt-auto flex flex-col gap-4">
-          <div className="grid grid-cols-3 gap-2 text-center mb-[10%]">
-            <div className={`bg-green-400 text-black font-bold ${boxClass}`}>
-              ⚡ กำลังชาร์จ
+      {/* Bottom Section */}
+      <div className="flex-grow bg-gradient-to-b from-teal-600 to-black text-white rounded-t-2xl shadow-lg p-4 flex flex-col">
+        <div className="text-left text-[5vw] leading-tight mb-1">
+          <p>
+            <span className="font-bold">ชื่อสถานี :</span> hello world
+          </p>
+          <p>
+            <span className="font-bold">หัวชาร์จ : </span>หัว1
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <div className="grid grid-cols-3 gap-2 text-center h-[40%] py-2">
+            {/* กล่อง 1: สถานะ */}
+            <div className="bg-gradient-to-b from-green-400 to-[#ecfcf9] text-black font-bold rounded-3xl px-3 py-2 flex flex-col h-full">
+              <div className="text-left text-base">สถานะ</div>
+              <div className="flex-1 flex items-center justify-center text-lg">
+                กำลังชาร์จ
+              </div>
             </div>
-            <div className={`bg-yellow-300 text-black font-bold ${boxClass}`}>
-              อุณหภูมิ <br />
-              50°C
+            <div className="bg-gradient-to-b from-yellow-300 to-[#ecfcf9] text-black font-bold rounded-3xl px-3 py-2 flex flex-col h-full">
+              <div className="text-left text-base">อุณหภูมิ</div>
+              <div className="flex-1 flex items-center justify-center text-lg">
+                50°C
+              </div>
             </div>
-            <div className={`bg-white text-black text-sm ${boxClass}`}>
-              เวลา
-              <br />
-              ชาร์จเต็มใน :
+
+            <div className="bg-black-100 text-black rounded-3xl px-3 py-2 flex flex-col h-full">
+              <div className="text-left text-base font-bold">เวลา</div>
+              <div className="flex-1 flex items-center justify-center text-lg">
+                ชาร์จเต็มใน :
+              </div>
             </div>
           </div>
 
           <div
-            className={`flex w-full text-black rounded-xl overflow-hidden ${boxClass}`}
+            className={`flex w-full h-[150px] text-black rounded-3xl overflow-hidden mb-5`}
             style={{
               backgroundImage: "linear-gradient(to bottom, #ecfcf9 , #008da9)",
             }}
@@ -58,13 +68,14 @@ export default function ChargingSession() {
             </div>
           </div>
 
-          <div className="text-xs text-center text-white/60">1 kWh / 4 บาท</div>
+          {/* <div className="text-xs text-center text-white/60">1 kWh / 4 บาท</div> */}
 
-          <button
-            className={`w-full bg-red-500 hover:bg-red-600 text-white text-lg font-bold ${boxClass}`}
-          >
-            หยุดการชาร์จ
-          </button>
+          <div className="w-[350px] h-[54px] mx-auto">
+            <RedButton
+              ButtonText="หยุดการชาร์จ"
+              onClick={() => console.log("hello tonklaman")}
+            />
+          </div>
         </div>
       </div>
     </div>
