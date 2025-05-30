647ebd069f652cce6ad42a7261a6b423fe05980d
asadawuth <122080707+asadawuth@users.noreply.github.com>
2025-05-30 15:37:42 +0700

    up

--- tonkla-1stnext-app/src/components/reusecomponents/RedButton.tsx
+++ tonkla-1stnext-app/src/components/reusecomponents/RedButton.tsx
@@ -16,15 +16,7 @@ export default function RedButton({
     "hover:shadow-[6px_6px_10px_rgba(0,0,0,0.4)] active:scale-95 " +
     "active:shadow-[inset_2px_2px_6px_rgba(0,0,0,0.3)]";
   return (
-<<<<<<< HEAD
     <button onClick={onClick} className={style ? style : defaultClass}>
-=======
-    <button
-      type="button"
-      onClick={onClick}
-      className="w-full h-[54px] hover:cursor-pointer bg-red-500 text-white font-bold text-[1.5rem] border-2 border-red-500 rounded-[18px] shadow-[4px_4px_8px_rgba(0,0,0,0.3)] transition-all duration-300 ease-in-out hover:bg-red-200 hover:shadow-[6px_6px_10px_rgba(0,0,0,0.4)] active:scale-95 active:shadow-[inset_2px_2px_6px_rgba(0,0,0,0.3)]"
-    >
->>>>>>> Tonkla
       {ButtonText}
     </button>
   );
